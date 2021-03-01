098b868415ddbbbab43b96c51babb399378ef4be
Marcus Ahlberg <marcus@aahlborg.se>
2021-03-01 22:53:21 +0100

    Add input data

--- qf.py
+++ qf.py
@@ -12,6 +12,10 @@ class Qf:
     def __init__(self, code):
         """Create a program from a string containing the code"""
         self.code = [c for c in code if c in self.INST]
+        self.r0 = 0
+        self.pc = 0
+        self.lr = []
+        self.memory = [0] * 256
 
     @staticmethod
     def from_bf(bf_code):
@@ -23,57 +27,66 @@ class Qf:
         """Print code"""
         return "".join(self.code)
 
+    def debug_print(self):
+        print(f"r0: {self.r0} ({self.memory[self.r0]})")
+        print(f"pc: {self.pc} ({self.code[self.pc]})")
+        print(f"lr: {self.lr}")
+
+    def step(self):
+        """Execute a single instruction"""
+        op = self.code[self.pc]
+
+        if op == self.INST[0]:
+            self.r0 -= 1
+            self.pc += 1
+        elif op == self.INST[1]:
+            self.r0 += 1
+            self.pc += 1
+        elif op == self.INST[2]:
+            self.memory[self.r0] -= 1
+            self.pc += 1
+        elif op == self.INST[3]:
+            self.memory[self.r0] += 1
+            self.pc += 1
+        elif op == self.INST[4]:
+            print(chr(self.memory[self.r0]), end="")
+            self.pc += 1
+        elif op == self.INST[5]:
+            try:
+                # Use package getch to read single character from user
+                import getch
+                self.memory[self.r0] = ord(getch.getch())
+                self.pc += 1
+            except:
+                print("Need package getch to read a character.")
+                print("Install using pip:")
+                print("  pip install getch")
+                exit(1)
+        elif op == self.INST[6]:
+            if self.memory[self.r0] != 0:
+                self.lr.append(self.pc)
+                self.pc += 1
+            else:
+                # Jump to end of loop
+                depth = 1
+                while depth > 0:
+                    self.pc += 1
+                    if self.code[self.pc] == self.INST[6]:
+                        depth += 1
+                    if self.code[self.pc] == self.INST[7]:
+                        depth -= 1
+                self.pc += 1
+        elif op == self.INST[7]:
+            self.pc = self.lr.pop()
+
     def run(self, data):
         """Execute the code on a data array"""
-        i = 0
-        pc = 0
-        lr = []
-        while pc < len(self.code):
-            op = self.code[pc]
-            if op == self.INST[0]:
-                i -= 1
-                pc += 1
-            elif op == self.INST[1]:
-                i += 1
-                pc += 1
-            elif op == self.INST[2]:
-                data[i] -= 1
-                pc += 1
-            elif op == self.INST[3]:
-                data[i] += 1
-                pc += 1
-            elif op == self.INST[4]:
-                print(chr(data[i]), end="")
-                pc += 1
-            elif op == self.INST[5]:
-                try:
-                    # Use getch package to read single character from user
-                    import getch
-                    data[i] = ord(getch.getch())
-                    pc += 1
-                except:
-                    print("Need getch package to do character input.")
-                    print("Install using pip:")
-                    print("  pip install getch")
-                    exit(1)
-            elif op == self.INST[6]:
-                if data[i] != 0:
-                    lr.append(pc)
-                    pc += 1
-                else:
-                    # Jump to end of loop
-                    depth = 1
-                    while depth > 0:
-                        pc += 1
-                        if self.code[pc] == self.INST[6]:
-                            depth += 1
-                        if self.code[pc] == self.INST[7]:
-                            depth -= 1
-                    pc += 1
-            elif op == self.INST[7]:
-                pc = lr.pop()
+        self.memory = data
 
-        return data
+        while self.pc < len(self.code):
+            self.step()
+
+        return self.memory
 
 def main():
     parser = argparse.ArgumentParser(description="A Quotefuck interpreter")
@@ -82,6 +95,7 @@ def main():
     parser.add_argument("-p", "--print_code", action="store_true", help="Print code and exit")
     parser.add_argument("-o", "--print_output", action="store_true", help="Print the memory after execution")
     parser.add_argument("-m", "--memory_size", type=int, default=256, help="Memory size")
+    parser.add_argument("-i", "--input_data", help="Input data, spearated by comma")
     args = parser.parse_args()
 
     with open(args.filename, "r") as f:
@@ -98,6 +112,11 @@ def main():
 
         # Create input array
         input = [0] * args.memory_size
+        if args.input_data:
+            data = args.input_data.split(",")
+            for i in range(len(data)):
+                input[i] = int(data[i])
+
         # Execute code
         output = qf.run(input)
 
